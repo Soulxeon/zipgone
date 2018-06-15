56f2977c456f3902e063beaf30a6251853f16ab2
Soulxeon <38793065+Soulxeon@users.noreply.github.com>
2018-06-15 12:17:22 -0400

    Add files via upload

---
+++ erasezip.py
@@ -0,0 +1,10 @@
+import os
+import time
+#seleccionar path
+path = "C:/Users/Elias/Downloads/erase"
+test = os.listdir(path)
+current_time = time.time()
+for item in test:
+    creation_time = os.path.getctime(path+"/"+item)
+    if item.endswith(".zip") and (current_time - creation_time) // (24 * 3600) >= 1:
+        os.remove(os.path.join(path, item))
